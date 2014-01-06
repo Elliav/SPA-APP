8f9cef50e256619add4bccbdb085bfe4237e8ca0
Elliav <omarboualam@193.49.105.53>
2014-01-06 14:08:16 +0100

    test10

--- S-Care.0.3/src/com/s_care/spa/MainActivity.java
+++ S-Care.0.3/src/com/s_care/spa/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends Activity {
        
 		Intent intent = new Intent(MainActivity.this, InterfaceQuestionEvaluation.class);		            
 		startActivity(intent);	
-
+//vjhfjfh
 //common
 	}
 
